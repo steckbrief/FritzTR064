2a011b79ee797a1591ee9ea72a0c1ba3158a5290
nick-less <nick-less@gmx.de>
2016-10-07 12:36:59 +0200

    added parse exception

---
+++ src/main/java/de/bausdorf/avm/tr064/ParseException.java
@@ -0,0 +1,10 @@
+package de.bausdorf.avm.tr064;
+
+public class ParseException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	
+	public ParseException (Exception cause) {
+		super (cause);
+	}
+}
